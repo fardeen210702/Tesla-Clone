d06827ea3bf91a0ccf381ab070cd7552ffbece83
Fardeen <fardeenmohd1251@gmail.com>
2024-01-07 00:47:03 +0530

    updated landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import Header from './components/Header'
 import './App.css'
 import '../src/responsiveness/phone.css'
-import AllVehicles from './components/AllVehicles'
+import VehiclesComp from './components/VehiclesComp'
 
 function App() {
 
   return (
     <div className='App'>
     <Header/>
+    <VehiclesComp/>
    
     
     </div>
---
+++ src/components/VehiclesComp.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Data from '../Data'
+
+function VehiclesComp() {
+  return (
+    <div className='vehiclescomp'>
+        <div className="vehiclesloop">
+            {
+                Data.map((el,index)=>{
+                    return <div className="vehiclecontainer" key={index}>
+                        <img src={el.mainImage} alt="" />
+                        <div className="btnsinfo1">
+                            <div className="cardetails">
+                                <h1>{el.title}</h1>
+                                <p>From ${el.price}*</p>
+                                <p>Lease starting at $329/mo*</p>
+                            </div>
+                            <div className="buttons">
+
+                            <div className="btns">
+                                <div className="btn1"> Explore Inventory</div>
+                                <div className="btn2">Demo Drive</div>
+                            </div>
+                            <p>*Excludes taxes and fees with price subject to change. Available in select states. <span>See Details</span></p>
+
+                            </div>
+
+
+
+                        </div>
+                    </div>
+                })
+
+            }
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default VehiclesComp
